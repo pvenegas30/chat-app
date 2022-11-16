51dc0daa345f84a43debd55ea076e96b0a20d9ba
Pedro <pedrovenegasjob@gmail.com>
2022-11-16 12:23:31 -0400

    Chat App 0.4

--- src/components/Chat.jsx
+++ src/components/Chat.jsx
@@ -1,11 +1,23 @@
 import React from 'react'
+// import { FaVideo, FaUserPlus, FaBars } from "react-icons/fa";
+import { BsList, BsPersonPlusFill, BsFillCameraVideoFill } from "react-icons/bs";
 
 const Chat = () => {
   return (
     <div className='chat'>
       
+      <div className="chatInfo">
+
+        <span>Jane</span>
+
+        <div className="chatIcons">
+          <BsFillCameraVideoFill/>
+          <BsPersonPlusFill/>
+          <BsList/>
+        </div>
+      </div>
       
-      Chat</div>
+    </div>
   )
 }
 
--- src/components/Chats.jsx
+++ src/components/Chats.jsx
@@ -1,10 +1,43 @@
-import React from 'react'
+import React from "react"
 
 const Chats = () => {
   return (
-    <div className='chats'>
-      
+    <div className="chats">
+      <div className="userChat">
+        <img
+          src="https://engineering.unl.edu/images/staff/Kayla-Person.jpg"
+          alt=""
+        />
 
+        <div className="userChatInfo">
+          <span>Jane</span>
+          <p>Hello</p>
+        </div>
+      </div>
+
+      <div className="userChat">
+        <img
+          src="https://engineering.unl.edu/images/staff/Kayla-Person.jpg"
+          alt=""
+        />
+
+        <div className="userChatInfo">
+          <span>Jane</span>
+          <p>Hello</p>
+        </div>
+      </div>
+
+      <div className="userChat">
+        <img
+          src="https://engineering.unl.edu/images/staff/Kayla-Person.jpg"
+          alt=""
+        />
+
+        <div className="userChatInfo">
+          <span>Jane</span>
+          <p>Hello</p>
+        </div>
+      </div>
 
     </div>
   )
